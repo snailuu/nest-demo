d6242d4c0c9be4409686e6e3cd1631154fe0509a
snailuu <67217277@qq.com>
2024-08-18 22:40:43 +0800

    fix: 解决循环依赖

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AaaModule } from './aaa/aaa.module';
+import { BbbModule } from './bbb/bbb.module';
+import { CccService } from './ccc.service';
+import { DddService } from './ddd.service';
 
 @Module({
-  imports: [],
+  imports: [AaaModule, BbbModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CccService, DddService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { CccService } from './ccc.service';
+import { DddService } from './ddd.service';
 
 @Injectable()
 export class AppService {
+  constructor(
+    private cccService: CccService,
+    private dddService: DddService,
+  ) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return this.dddService.ddd() + this.cccService.eee();
   }
 }
