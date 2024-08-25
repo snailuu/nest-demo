4893662a0cf128a43f4a6356993de4c306411e10
snailuu <67217277@qq.com>
2024-08-25 20:42:50 +0800

    feat: 添加了AaaInterceptor和其他拦截器，以及对应的控制器方法

---
+++ src/tap-test.interceptor.ts
@@ -0,0 +1,24 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+import { AppService } from './app.service';
+
+@Injectable()
+export class TapTestInterceptor implements NestInterceptor {
+  constructor(private appService: AppService) {}
+
+  private readonly logger = new Logger(TapTestInterceptor.name);
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      tap((data) => {
+        this.appService.getHello();
+        this.logger.log(`log somthing`, data);
+      }),
+    );
+  }
+}
