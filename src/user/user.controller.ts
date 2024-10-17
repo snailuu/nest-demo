828ea4b2f5ba5f3f59bd0f573663cf0d7d4c1fdf
snailuu <67217277@qq.com>
2024-10-17 13:20:07 +0800

    fix: 提交修改

---
+++ src/user/user.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { RegisterUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  async register(@Body() registerUserDto: RegisterUserDto) {
+    return this.userService.register(registerUserDto);
+  }
+
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto) {
+    return this.userService.login(loginUserDto);
+  }
+
+  @Post()
+  create(@Body() createUserDto: RegisterUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@nestjs/common';
+import { RegisterUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { DbService } from 'src/db/db.service';
+import { Inject } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { BadRequestException } from '@nestjs/common';
+import { LoginUserDto } from './dto/login-user.dto';
+
+@Injectable()
+export class UserService {
+  @Inject(DbService)
+  dbService: DbService;
+  create(createUserDto: RegisterUserDto) {
+    return 'This action adds a new user';
+  }
+  async register(registerUserDto: RegisterUserDto) {
+    const users: User[] = await this.dbService.read();
+
+    const foundUser = users.find(
+      (item) => item.username === registerUserDto.username,
+    );
+
+    if (foundUser) {
+      throw new BadRequestException('该用户已经注册');
+    }
+
+    const user = new User();
+    user.username = registerUserDto.username;
+    user.password = registerUserDto.password;
+    users.push(user);
+
+    await this.dbService.write(users);
+    return user;
+  }
+  async login(loginUserDto: LoginUserDto) {
+    const users: User[] = await this.dbService.read();
+
+    const foundUser = users.find(
+      (item) => item.username === loginUserDto.username,
+    );
+
+    if (!foundUser) {
+      throw new BadRequestException('用户不存在');
+    }
+
+    if (foundUser.password !== loginUserDto.password) {
+      throw new BadRequestException('密码不正确');
+    }
+
+    return foundUser;
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
