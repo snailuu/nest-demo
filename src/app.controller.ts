950a7a369c908b32e64abd05144536ae9c27ed77
snailuu <huangzhuofeng@idea.edu.cn>
2024-11-18 11:38:15 +0800

    feat: 日志

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MyLogger } from './MyLogger';
+import { WINSTON_LOGGER_TOKEN } from './winston/winston.module';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Inject(WINSTON_LOGGER_TOKEN)
+  private logger;
+
   @Get()
   getHello(): string {
+    this.logger.log('getHello', AppController.name);
     return this.appService.getHello();
   }
 }
