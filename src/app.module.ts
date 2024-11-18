3755dfb189c353a26b06ea88b6a9085b4f18723c
snailuu <huangzhuofeng@idea.edu.cn>
2024-11-18 18:10:57 +0800

    feat: rbac

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,58 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { Role } from './user/entities/role.eneity';
+import { User } from './user/entities/user.entity';
+import { Permission } from './user/entities/permission.entity';
+import { AaaModule } from './aaa/aaa.module';
+import { BbbModule } from './bbb/bbb.module';
+import { APP_GUARD } from '@nestjs/core';
+import { LoginGuard } from './login.guard';
+import { JwtModule } from '@nestjs/jwt';
+import { PermissionGuard } from './permission.guard';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "123456",
+      database: "rbac_test",
+      synchronize: true,
+      logging: true,
+      entities: [User, Role, Permission],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+          authPlugin: 'sha256_password',
+      }
+    }),
+    UserModule,
+    AaaModule,
+    BbbModule,
+    JwtModule.register({
+      global: true,
+      secret: 'your-secret-key',
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: LoginGuard
+    },
+    {
+      provide: APP_GUARD,
+      useClass: PermissionGuard
+    }
+  ],
 })
 export class AppModule {}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,64 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Inject } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { JwtService } from '@nestjs/jwt';
+
+@Controller('user')
+export class UserController {
+
+  @Inject(JwtService)
+  private jwtService: JwtService;
+
+
+  constructor(private readonly userService: UserService) { }
+  @Get('init')
+  async initData() {
+    await this.userService.initData();
+    return 'done';
+  }
+
+  @Post('login')
+async login(@Body() loginUser: UserLoginDto){
+  const user = await this.userService.login(loginUser);
+
+  const token = this.jwtService.sign({
+    user: {
+      username: user.username,
+      roles: user.roles
+    }
+  });
+
+  return {
+      token
+  }
+}
+
+
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+}
