21c0d9856e5fa194f50bffac146f2ecec2179926
snailuu <huangzhuofeng@idea.edu.cn>
2024-11-18 15:43:59 +0800

    fix: jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    JwtModule.register({
+      secret: 'snailuu',
+      signOptions: {
+        expiresIn: '7d'
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
