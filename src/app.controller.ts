227cbe6600b0fb6e5a0dce08ba274a17fc432cbb
snailuu <huangzhuofeng@idea.edu.cn>
2024-11-19 15:38:51 +0800

    feat: swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,81 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post, Query, UnauthorizedException } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiBasicAuth, ApiBearerAuth, ApiBody, ApiCookieAuth, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CccDto } from './ccc.dto';
+import { CccVo } from './ccc.vo';
 
+@ApiTags('测试')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @ApiBearerAuth('bearer')
+  @ApiOperation({ summary: '测试 aaa', description: 'aaa 描述' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'aaa 成功',
+    type: String
+  })
+  @Get('aaa')
+  aaa(@Query('a1') a1, @Query('a2') a2) {
+    console.log(a1, a2);
+    return 'aaa success';
+  }
+
+
+  @ApiCookieAuth('cookie')
+  @ApiOperation({ summary: '测试 bbb', description: 'bbb 描述' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'bbb 成功',
+    type: String
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'id 不合法'
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'ID',
+    required: true,
+    example: 222,
+  })
+  @Get('bbb/:id')
+  bbb(@Param('id') id: number) {
+    console.log(id);
+    if (id !== 111) {
+      throw new UnauthorizedException();
+    }
+    return 'bbb success';
+  }
+
+
+  @ApiBasicAuth('basic')
+  @ApiOperation({ summary: '测试 ccc' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'ccc 成功',
+    type: CccVo
+  })
+  @ApiBody({
+    type: CccDto
+  })
+  @Post('ccc')
+  ccc(@Body('ccc') ccc: CccDto) {
+    console.log(ccc);
+
+    const vo = new CccVo();
+    vo.aaa = 111;
+    vo.bbb = 222;
+    return vo;
+  }
+
+
+
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Test example')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('test')
+    .addBasicAuth({
+      type: 'http',
+      name: 'basic',
+      description: '用户名 + 密码'
+    })
+    .addCookieAuth('sid', {
+      type: 'apiKey',
+      name: 'cookie',
+      description: '基于 cookie 的认证'
+    })
+    .addBearerAuth({
+      type: 'http',
+      description: '基于 jwt 的认证',
+      name: 'bearer'
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
